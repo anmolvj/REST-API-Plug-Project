fba3c7a8cfdd0f488f91ce19af724402f54fa618
Anmol Vijayvargiya <anmol.vijay94@gmail.com>
2017-03-14 23:07:09 -0700

    updated server with all pin paths

--- server.js
+++ server.js
@@ -6,8 +6,15 @@ var app			= express();
 var port 		= 3000;
 
 var index 		= require('./routes/index');
-var mypath 		= require('./routes/path');
-
+var pin4 		= require('./routes/pin4');
+var pin5 		= require('./routes/pin5');
+var pin2 		= require('./routes/pin2');
+var pin16 		= require('./routes/pin16');
+var pin0		= require('./routes/pin0');
+var pin15 		= require('./routes/pin15');
+var pin13 		= require('./routes/pin13');
+var pin12 		= require('./routes/pin12');
+var pin14 		= require('./routes/pin14');
 
 app.disable('x-powered-by');
 
@@ -23,15 +30,20 @@ app.engine('html',require('ejs').renderFile);
 
 //------------------------ ROUTES --------------------
 app.use('/', index);
-app.use('/path', mypath);
-
+app.use('/pin4', pin4);
+app.use('/pin5', pin5);
+app.use('/pin2', pin2);
+app.use('/pin16', pin16);
+app.use('/pin0', pin0);
+app.use('/pin15', pin15);
+app.use('/pin13', pin13);
+app.use('/pin12', pin12);
+app.use('/pin14', pin14);
 
 //------------------------PORT LISTENING CODE--------------------
 
 app.listen(port, function(){
 	console.log('Server started on port:'+port);
 	});
-// app.set('port', process.env.PORT || 3000);
-// 
-// ------------------------PORT LISTEN ENDS----------------------
+
 
